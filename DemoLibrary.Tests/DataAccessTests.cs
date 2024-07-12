7bfcfdad1d84967f76b4eaec35c15e35885dc4fb
Rehaan1 <rehaanmazid@gmail.com>
2024-07-12 11:44:56 +0530

    feat: tests for dataaccess functions

---
+++ DemoLibrary.Tests/DataAccessTests.cs
@@ -0,0 +1,35 @@
+﻿using DemoLibrary.Models;
+using System.Collections.Generic;
+using Xunit;
+
+namespace DemoLibrary.Tests
+{
+    public class DataAccessTests
+    {
+        [Fact]
+        public void AddPersonToPeopleList_WhenCalled_ShouldAddToPersonList()
+        {
+            // Arrange
+            List<PersonModel> people = new List<PersonModel>();
+            PersonModel newPerson = new PersonModel { FirstName = "Rehaan", LastName = "Mazid" };
+
+            // Act
+            DataAccess.AddPersonToPeopleList(people, newPerson);
+
+            // Assert - Only passes if both the assertions are true
+            Assert.True(people.Count == 1);
+            Assert.Contains<PersonModel>(newPerson, people);
+        }
+
+        [Theory]
+        [InlineData("Rehaan", "", "LastName")]
+        [InlineData("", "Mazid", "FirstName")]
+        public void AddPersonToPeopleList_InvalidPerson_ShouldThrowException(string firstName, string lastName, string paramName)
+        {
+            List<PersonModel> people = new List<PersonModel>();
+            PersonModel newPerson = new PersonModel { FirstName = firstName, LastName = lastName };
+
+            Assert.Throws<System.ArgumentException>(paramName, () => DataAccess.AddPersonToPeopleList(people, newPerson));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 - To test, go to Test > Test Explorer. It should fill up with the XUnit Test and Classes you have created
 - A Unit Test is a test that does not run with any external systems like Databases, etc
+- In Unit Tests you can use Mocking to simulate databases, etc but you have to be really careful
 - An Integration Test is a test that runs with external systems
 - An End-To-End Test is a test that runs the entire program including UI clicks, etc
